9ddafcff22a6a324cd3f826d36802860b429ef6b
Pablo Buey Martín <94113076+pablobuey@users.noreply.github.com>
2025-03-14 15:29:12 +0100

    implementación interfaces

--- BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/UsuarioJPARepository.java
+++ BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/UsuarioJPARepository.java
@@ -1,11 +1,39 @@
 package com.banana.bananawhatsapp.persistencia;
 
+import com.banana.bananawhatsapp.modelos.Usuario;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Repository;
 
+import java.sql.SQLException;
+import java.util.Set;
+
 @Setter
 @Getter
 @Repository
-public class UsuarioJPARepository {
+public class UsuarioJPARepository implements IUsuarioRepository{
+    @Override
+    public Usuario obtener(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Usuario crear(Usuario usuario) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Usuario actualizar(Usuario usuario) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean borrar(Usuario usuario) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Set<Usuario> obtenerPosiblesDestinatarios(Integer id, Integer max) throws SQLException {
+        return Set.of();
+    }
 }
