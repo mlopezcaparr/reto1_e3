f5b829d46dfdc652aff13e2fcba3eb495b4936e5
AramburuL <maramburu280@gmail.com>
2025-03-14 13:43:42 +0100

    Implementacion spring

--- BananaWhatsApp/src/test/java/com/banana/bananawhatsapp/BananaWhatsAppApplicationTest.java
+++ BananaWhatsApp/src/test/java/com/banana/bananawhatsapp/BananaWhatsAppApplicationTest.java
@@ -2,15 +2,26 @@ package com.banana.bananawhatsapp;
 
 import com.banana.bananawhatsapp.util.DBUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/*@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {SpringConfig.class})*/
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringConfig.class})
+@EnableAutoConfiguration
 class BananaWhatsAppApplicationTest {
+    @PersistenceContext
+    EntityManager em;
     @Test
-    public void load() {
-        DBUtil.reloadDB();
+    public void load(){
+        assertNotNull(em);
         assertTrue(true);
     }
 }
